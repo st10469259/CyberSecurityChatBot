adad765172923c23a5408a6f18a42309baa3d85a
st10469259 <ntsikamathomane66@gmail.com>
2026-04-13 23:42:55 +0200

    Update Greeting.cs
    add comments

--- Greeting.cs
+++ Greeting.cs
@@ -19,7 +19,7 @@ namespace CyberSecurityChatBot
                 //auto get the path of the voice record
                 string path = AppDomain.CurrentDomain.BaseDirectory;
 
-                //rename path
+                //rename the path
                 string fullpath = path.Replace(@"bin\Debug\", "greeting.wav");
 
                 //load audio, than play the audio after instance
